e74814b5cd21d24f0c0d39ddfa639924729706e9
Fredrik Lindeborg <fredrik.lindeborg@starstable.com>
2018-09-20 11:06:17 +0200

    adding project

---
+++ crawler.py
@@ -0,0 +1,84 @@
+from bs4 import BeautifulSoup
+from urllib.parse import quote_plus
+from urllib.request import urlopen
+import random
+
+
+class Crawler(object):
+
+    def _crawl(
+        self, url, search, space_delimiter, recipe_tag,
+        recipe_class, source, img_prefix,
+        title_tag, title_class, img_class, results
+    ):
+        search = search.replace(" ", space_delimiter)
+        search = quote_plus(search)
+        url = url.replace("%search%", search)
+        try:
+            soup = BeautifulSoup(urlopen(url, timeout=5), 'html.parser')
+        except:
+            print(url, " FAILED TO LOAD")
+            return
+        else:
+            print(url, " scanned")
+        recipes = soup.find_all(recipe_tag, recipe_class)
+        for recipe in recipes:
+            result = {}
+
+            if img_class:
+                img = recipe.find('img', class_=img_class)
+            else:
+                img = recipe.find('img')
+            if img:
+                img_src = img.attrs.get('data-original-src')
+                if not img_src:
+                    img_src = img.attrs.get('data-src')
+                if not img_src:
+                    img_src = img.attrs.get('src')
+                result['img'] = img_prefix + img_src
+            if title_tag and title_class:
+                title = recipe.find(title_tag, class_=title_class)
+            else:
+                title = recipe.find('h2')
+                if not title:
+                    title = recipe.find('h3')
+            link = recipe.find('a')
+            result['link'] = link.attrs['href']
+            result['title'] = title.string
+            result['source'] = source
+            results.append(result)
+        return results
+
+    def fetch(self, search):
+        results = []
+
+        self._crawl(
+            url='https://www.allrecipes.com/search/results/?wt=%search%&sort=re',
+            search=search,
+            space_delimiter="%20",
+            recipe_tag="article",
+            recipe_class="fixed-recipe-card",
+            source="allrecipes.com",
+            img_prefix="",
+            title_tag="span",
+            title_class="fixed-recipe-card__title-link",
+            img_class="fixed-recipe-card__img",
+            results=results
+        )
+
+        self._crawl(
+            url='https://www.jamieoliver.com/search/?s=%search%',
+            search=search,
+            space_delimiter="+",
+            recipe_tag="div",
+            recipe_class="recipe",
+            source="jamieoliver.com",
+            img_prefix="",
+            titel_tag="",
+            title_class="",
+            img_class="img-responsive",
+            results=results
+        )
+
+        random.shuffle(results)
+        return results
---
+++ main.py
@@ -0,0 +1,39 @@
+import cherrypy
+from mako.template import Template
+import crawler
+import os
+
+
+class Main(object):
+    @cherrypy.expose
+    def index(self, *args, **kwargs):
+        data = {}
+        if kwargs.get('search'):
+            c = crawler.Crawler()
+            r = c.fetch(kwargs['search'])
+            data['recipes'] = r
+            data['search'] = kwargs['research']
+        mytemplate = Template(filename='search.html')
+        return mytemplate.render(**data)
+
+
+cherrypy.config.update({
+    'server.socket_port': 8080,
+    'tools.encode.on': True,
+    'tools.encode.encoding': 'utf-8'
+    })
+conf = {
+    '/': {'tools.staticdir.root': os.path.abspath(os.getcwd())},
+    '/css': {
+        'tools.staticdir.on':True,
+        'tools.staticdir.dir':"css"
+    },
+    '/js': {
+        'tools.staticdir.on':True,
+        'tools.staticdir.dir':"js"
+    }
+}
+if __name__ == '__main__':
+    cherrypy.tree.mount(Main(), '/', conf)
+    cherrypy.engine.start()
+    cherrypy.engine.block()
